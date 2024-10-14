be4391bbf07127bfefcaffcacb7fd2537209b71d
Michael Pabst <mikeypbr3@gmail.com>
2024-10-14 10:11:49 -0400

    attempting to download just python 3.8.0 instead of 3.8.12 again. Maybe it will work this time.

--- build.sh
+++ build.sh
@@ -14,38 +14,46 @@ nvm use 14
 
 # Install Python 3.8
 echo "Downloading Python 3.8..."
-curl -O https://www.python.org/ftp/python/3.8.12/Python-3.8.12.tgz 
+curl -O https://www.python.org/ftp/python/3.8.0/Python-3.8.0.tgz
 echo "Verifying download..."
-if ! tar tzf Python-3.8.12.tgz >/dev/null; then
+if ! tar tzf Python-3.8.0.tgz >/dev/null; then
     echo "Download corrupted, retrying..."
-    curl -O https://www.python.org/ftp/python/3.8.12/Python-3.8.12.tgz
-    if ! tar tzf Python-3.8.12.tgz >/dev/null; then
+    curl -O https://www.python.org/ftp/python/3.8.0/Python-3.8.0.tgz
+    if ! tar tzf Python-3.8.0.tgz >/dev/null; then
         echo "Download failed again. Exiting..."
         exit 1
     fi
 fi
-tar xzf Python-3.8.12.tgz
-cd Python-3.8.12
+tar xzf Python-3.8.0.tgz
+cd Python-3.8.0
 ./configure --prefix=$HOME/.localpython --with-tcltk-includes='-I/usr/include' --with-tcltk-libs='-L/usr/lib -ltcl8.6 -ltk8.6' LDFLAGS="-lgcc_s"
 make
 make install
 cd ..
+
+# Add local Python to PATH
 export PATH=$HOME/.localpython/bin:$PATH
+
+# Verify Python installation
 python3.8 --version
 
-# virtual environment
+# Create virtual environment
 echo "Creating virtual environment..."
 python3.8 -m venv venv
+
+# Activate virtual environment
 source venv/bin/activate
 
-# python stuff
+# Normal Python setup
 python --version
 echo "Upgrading pip..."
 python -m pip install --upgrade pip
+
+# Install required Python packages
 echo "Installing dependencies..."
 pip install -r requirements.txt
 
-# copy things
+# Copy everything to the Netlify functions directory
 mkdir -p .netlify/functions/python
 cp -r venv .netlify/functions/
 cp -r $HOME/.localpython .netlify/functions/python/
